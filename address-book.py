f4acb776c3a45d3d4cbccd090624556a9d403b4c
AG Tribbs <andrewagrover@gmail.com>
2023-10-09 18:23:36 -0500

    Initial Commit

---
+++ address-book.py
@@ -0,0 +1,9 @@
+###
+#   address-book.py
+#
+# Requirements:
+#   1. Add a new contact with a name, pgone number, and email address
+#   2. View a list of all contacts in the address book
+#   3. Search for a contact by name and display their information
+#   4. Update contact information (phone number and email) based on the name
+#   5. Delete a contact name
